69df31b40de74f3015cbb1092fcc7604097ae355
wsss777 <2484694349@qq.com>
2026-03-18 16:51:17 +0800

    简单图片识别

--- backend/internal/common/rabbitmq/consumer.go
+++ backend/internal/common/rabbitmq/consumer.go
@@ -10,27 +10,11 @@ import (
 // ConsumeWork 启动消费者（Work 模式）
 func (r *RabbitMQ) ConsumeWork(handle func(msg *amqp.Delivery) error) error {
 	for {
-
-		//if r.channel == nil {
-		//	return fmt.Errorf("channel is nil")
-		//}
 		if err := r.channel.Qos(1, 0, false); err != nil {
 			logger.L().Error("RabbitMQ set QoS failed", zap.Error(err))
 			r.reconnect()
 			continue
 		}
-
-		//q, err := r.channel.QueueDeclare(
-		//	r.queueName, false, false, false, false, nil,
-		//)
-		//if err != nil {
-		//	logger.L().Error("RabbitMQ queue declare failed in consume",
-		//		zap.Error(err),
-		//		zap.String("queue", r.queueName),
-		//	)
-		//	r.reconnect()
-		//	continue
-		//}
 
 		msgs, err := r.channel.Consume(
 			r.queueName, "", false, false, false, false, nil,
@@ -49,11 +33,14 @@ func (r *RabbitMQ) ConsumeWork(handle func(msg *amqp.Delivery) error) error {
 
 		for msg := range msgs {
 			if err := handle(&msg); err != nil {
+				msg.Nack(false, true)
 				logger.L().Error("RabbitMQ message handle failed",
 					zap.Error(err),
 					zap.ByteString("body", msg.Body),
 					zap.String("queue", r.queueName),
 				)
+			} else {
+				msg.Ack(false)
 			}
 		}
 		logger.L().Warn("RabbitMQ channel closed, reconnecting...")
